145f2dc2f3e63315f48c98ea8dcf4527c15e656f
egyptianf <walied2@protonmail.com>
2021-09-25 20:17:43 +0200

    add the word "connected" to the button text.

--- src/sample/ChatServerEndpoint.java
+++ src/sample/ChatServerEndpoint.java
@@ -20,7 +20,7 @@ public class ChatServerEndpoint {
     public static Session mySession;
 
 
-    public static SimpleStringProperty connectedNumProperty = new SimpleStringProperty("0");
+    public static SimpleStringProperty connectedNumProperty = new SimpleStringProperty("0"+"\nCONNECTED");
 
 
     @OnOpen
@@ -29,7 +29,7 @@ public class ChatServerEndpoint {
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
-                connectedNumProperty.set(String.valueOf(Integer.parseInt(connectedNumProperty.getValue())+1) );
+                connectedNumProperty.set(String.valueOf(Integer.parseInt(connectedNumProperty.getValue())+1)+"\nCONNECTED");
             }
         });
         mySession = session;
@@ -60,7 +60,7 @@ public class ChatServerEndpoint {
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
-                connectedNumProperty.set(String.valueOf(Integer.parseInt(connectedNumProperty.getValue())-1));
+                connectedNumProperty.set(String.valueOf(Integer.parseInt(connectedNumProperty.getValue())-1)+"\nCONNECTED");
             }
         });
         try {
